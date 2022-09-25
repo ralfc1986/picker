19d630bf840ebddac59f3ca80f37249e061b3196
Ralf C <chin_huang@ymail.com>
2022-09-25 14:31:38 +0800

    rename pick attr to forecast

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,6 +1,6 @@
 class Pick < ApplicationRecord  
   belongs_to :user
   belongs_to :match
-  belongs_to :prediction,    class_name: "Outcome", primary_key:  :code, foreign_key: :outcome_code, optional: true
+  belongs_to :forecast,    class_name: "Outcome", primary_key:  :code, foreign_key: :outcome_code, optional: true
 end
   
