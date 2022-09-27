9a91ad0fd93c16cb538319e7bc9ce961c0a19cb9
Ralf C <chin_huang@ymail.com>
2022-09-27 23:02:47 +0800

    Validate table processing

--- app/models/league_by_day.rb
+++ app/models/league_by_day.rb
@@ -2,6 +2,8 @@ class LeagueByDay < ApplicationRecord
   belongs_to :day
   belongs_to :user
 
+  scope :today,   -> { joins(:day).merge(Day.today)}
+  scope :yesterday,   -> { joins(:day).merge(Day.yesterday)}
   scope :on_day, -> (inp_day) { where(day: inp_day)}
   scope :by_user, -> (user_id) { where(users: user_id)}
   scope :on_day_by_user, -> (inp_day, user_id) { where(day: inp_day, users: user_id)}
@@ -13,9 +15,40 @@ class LeagueByDay < ApplicationRecord
     day_matches = day_obj.matches
     # return if none of the matches on the day has result
     return if day_matches.count == day_matches.where(outcome_code: nil).count
+    process_picks_score(day_matches)
+    # Update total score for each user into the league_by_day table
+    users_daily_score_hash = day_obj.picks.group(:user_id).sum(:score)
+    users_daily_score_hash.each do |i|
+      existing_record = self.where(day_id: day, user_id: i.first).first
+      # create_or_update replacement
+      if existing_record.present?
+        existing_record.update!(points: i.last)
+      else
+        self.create!(day_id: day, user_id: i.first, points: i.last )
+      end      
+    end
+    # Trigger to update the league_master table
+    LeagueMaster.process_scores
+  end
+
+  def self.daily_mvps(day)
+    daily_table = self.on_day(day)
+    # points that is nil means table has not process before
+    return if daily_table.nil? || daily_table.where(points: nil).exists?
+    highest_points = daily_table.maximum(:points)
+    # daily_table.joins(:user).where(points: highest_points)
+    User.joins(:league_by_days).where(league_by_days: {points: highest_points, day_id: day})
+  end
+
+  def self.get_user_points_hash(day)
+    self.on_day(day).pluck(:user_id, :points).to_h
+  end
+    
+  private
+
+  def self.process_picks_score(day_matches)
     # weightage for all the matches within the day is the same
     weightage = day_matches.first.stage.weighting
-
     # process the all the picks points
     day_matches.order(:started_at).each do |match|
       # does not include match that has not started
@@ -29,25 +62,8 @@ class LeagueByDay < ApplicationRecord
       match.picks.update_all(score: -weightage)
       # Scope all the winner picks and update the points at 1 go
       match.picks.by_winners.update_all(score: winner_points)
-    end
-
-    users_daily_score_hash = day_obj.picks.group(:user_id).sum(:score)
-
-    users_daily_score_hash.each do |i|
-      self.create(day_id: day, user_id: i.first, points: i.last )
-    end
-
-    LeagueMaster.process_scores
+    end    
   end
 
-  def self.daily_mvps(day)
-    daily_table = self.on_day(day)
-    # points that is nil means table has not process before
-    return if daily_table.nil? || daily_table.where(points: nil).exists?
-    highest_points = daily_table.maximum(:points)
-    # daily_table.joins(:user).where(points: highest_points)
-    User.joins(:league_by_days).where(league_by_days: {points: highest_points, day_id: day})
-  end
-    
 end
   
--- app/models/league_master.rb
+++ app/models/league_master.rb
@@ -3,41 +3,63 @@ class LeagueMaster < ApplicationRecord
   belongs_to :user
 
   default_scope      {order(position: :asc)}
+  scope :today,   -> { joins(:day).merge(Day.today)}
+  scope :yesterday,   -> { joins(:day).merge(Day.yesterday)}
   scope :on_day,  -> (inp_day) { where(day: inp_day)}
   scope :by_user, -> (user_id) { where(users: user_id)}
   scope :on_day_by_user, -> (inp_day, user_id) { where(day: inp_day, users: user_id)}
 
   def self.process_scores
-    # Calculate the league table till yesterday(where all matches before is done)
-    # day_no = Day.where(match_date: Date.today).first.match_day - 1 if day_no.nil?
-    # Calculate from scratch up(process heavy)
-    day_no = Day.today.try(:match_day)
-    return if day_no.nil?
-    for i in 1..day_no do 
-      league_on_day = LeagueByDay.on_day(i)
-      return if league_on_day.nil? 
-      prev_league_on_day = i>1 ? LeagueByDay.on_day(i-1) : nil
-      prev_league_on_day_hash = prev_league_on_day.present? ? prev_league_on_day.pluck(:user_id, :points).to_h : nil
-      league_on_day.pluck(:user_id, :points).to_h.each do |k,v|
-        points = prev_league_on_day.present? ? (v + prev_league_on_day_hash[k]) : v
-        if self.where(day_id: i, user_id: k).present?
-          self.update(day_id: i, user_id: k, points: points) 
-        else
-          self.create(day_id: i, user_id: k, points: points) 
-        end
-      end
-      prev_league_master = i>1 ? self.on_day(i-1) : nil
-      prev_league_master.pluck(:user_id, :points).to_h if prev_league_on_day.present?
-      prev_league_master_hash = prev_league_master.present? ? prev_league_master.pluck(:user_id, :position).to_h : nil
-      for pos in 1..self.on_day(i).count
-        current_top = self.on_day(i).where(position: nil).order(points: :desc).first
-        current_top.update_columns(position: pos)
-        if i>1
-          prev_position = prev_league_master_hash[current_top.user_id]          
-          current_top.update_columns(position_movement: ( prev_position - pos))
-        end
+    # Calculate the league table till today
+    day_until = Day.today.try(:match_day)    
+    return if day_until.nil?
+    # Re-calculate from day 1 up
+    for day in 1..day_until do 
+      process_points_by_day(day)
+      process_position_by_day(day)     
+    end
+  end
+
+  private
+
+  def self.process_points_by_day(day)
+    league_on_day_hash = LeagueByDay.get_user_points_hash(day)
+    return if league_on_day_hash.nil? 
+    prev_league_on_day_hash = day>1 ?  self.get_user_points_hash(day-1) : nil      
+    league_on_day_hash.each do |k,v|
+      points = prev_league_on_day_hash.present? ? (v + prev_league_on_day_hash[k]) : v
+      existing_record = self.where(day_id: day, user_id: k).first
+      # create_or_update replacement
+      if existing_record.present?
+        existing_record.update!(points: points) 
+      else
+        self.create!(day_id: day, user_id: k, points: points) 
       end
     end
+  end
+
+  def self.process_position_by_day(day)    
+    league_master_on_day = self.on_day(day)
+    return if league_master_on_day.nil? || league_master_on_day.where(points: nil).exists?
+    # Reset all position for league_master on day
+    league_master_on_day.update_all(position: nil, position_movement: nil)
+    # Get the placement for the day before
+    prev_league_master_hash = day>1 ?  self.get_user_position_hash(day-1) : nil
+    for position in 1..league_master_on_day.count
+      current_top = league_master_on_day.where(position: nil).order(points: :desc).first
+      # Update current position
+      current_top.update_columns(position: position)
+      # Update position movement compare to position in previous day
+      current_top.update_columns(position_movement: ( prev_league_master_hash[current_top.user_id] - position))  if prev_league_master_hash.present?      
+    end
+  end
+
+  def self.get_user_points_hash(day)
+    self.on_day(day).pluck(:user_id, :points).to_h
+  end
+
+  def self.get_user_position_hash(day)
+    self.on_day(day).pluck(:user_id, :position).to_h
   end
 
 end
